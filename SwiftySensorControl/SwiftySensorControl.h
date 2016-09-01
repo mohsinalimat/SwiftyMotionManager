902618ae2133e7b216fb626b6ec8e785426645d1
Jeff Kang <sugar1230@gmail.com>
2016-09-01 21:34:50 +0900

    initial commit

---
+++ SwiftySensorControl/SwiftySensorControl.h
@@ -0,0 +1,19 @@
+//
+//  SwiftySensorControl.h
+//  SwiftySensorControl
+//
+//  Created by Jeff Kang on 9/1/16.
+//  Copyright © 2016 jeffgukang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftySensorControl.
+FOUNDATION_EXPORT double SwiftySensorControlVersionNumber;
+
+//! Project version string for SwiftySensorControl.
+FOUNDATION_EXPORT const unsigned char SwiftySensorControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftySensorControl/PublicHeader.h>
+
+
