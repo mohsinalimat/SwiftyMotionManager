cf9141f7c796579c4b0c6202f898edb01bf4637d
Jeff Kang <sugar1230@gmail.com>
2016-09-04 20:55:10 +0900

    Accelerometer example added.

---
+++ Example/Example/AccelViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Jeff Kang on 9/1/16.
+//  Copyright © 2016 jeffgukang. All rights reserved.
+//
+
+import UIKit
+import SwiftySensorControl
+import CoreMotion
+
+class AccelViewController: UIViewController, SwiftySensorControlDelegate {
+
+    
+    @IBOutlet weak var xLabel: UILabel!
+    @IBOutlet weak var yLabel: UILabel!
+    @IBOutlet weak var zLabel: UILabel!
+    
+    let sensorControl = SwiftySensorControl()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        sensorControl.delegate = self
+        sensorControl.startAccelelometerUpdatesToMainQueue()
+}
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func accelerometerUpdated(accellerometerData: CMAccelerometerData?) {
+//        accellerometerData?.timestamp.description
+        xLabel.text = accellerometerData?.acceleration.x.description
+        yLabel.text = accellerometerData?.acceleration.y.description
+        zLabel.text = accellerometerData?.acceleration.z.description
+    }
+}
