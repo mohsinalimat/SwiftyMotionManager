941aeb812575bd75a8a7c402dedbf6b35a8d8692
Jeff Kang <sugar1230@gmail.com>
2016-09-04 20:55:22 +0900

    Playground added.

---
+++ Playground.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import SwiftySensorControl
+
+var str = "Hello, playground"
+
+let control = SwiftySensorControl()
+control.startAccelerometerUpdatesIfItCan()
+
