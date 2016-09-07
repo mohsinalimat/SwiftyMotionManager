0ae329c0a6a95e0dc345a8b6d7b9fb961b3b64e4
Jeff Kang <sugar1230@gmail.com>
2016-09-07 22:53:04 +0900

    Features for gyro added. integratedValue added for CMAcceleration and CMRotationRate.

--- SwiftySensorControl/SwiftySensorControl.swift
+++ SwiftySensorControl/SwiftySensorControl.swift
@@ -21,9 +21,10 @@ public class SwiftySensorControl: CMMotionManager {
         print(self.deviceMotionAvailable)
     }
     
+    
     // MARK: Accelerometer
     
-    public func startAccelerometerUpdatesIfItCan() -> Bool{
+    public func startAccelerometerUpdatesIfAvailable() -> Bool{
         if !self.accelerometerAvailable {
             return false
         } else {
@@ -40,7 +41,7 @@ public class SwiftySensorControl: CMMotionManager {
         }
     }
 
-    /** Accelerometer data will treat by handler when it update.
+    /** Accelerometer data will treat by handler whenever it updates.
      */
     public func startAccelelometerUpdatesToMainQueue(handler: CMAccelerometerHandler) -> Bool {
         if !self.accelerometerAvailable {
@@ -51,9 +52,62 @@ public class SwiftySensorControl: CMMotionManager {
         }
     }
     
-    // TODO: Normalize x, y, z
+    
+    // MARK: Gyroscope
+    
+    public func startGyroUpdatesIfAvailable() -> Bool {
+        if !self.gyroAvailable {
+            return false
+        } else {
+            super.startGyroUpdates()
+            return true
+        }
+    }
+    
+    /** Gyro data transfer to delegate.
+     */
+    public func startGyroUpdatesToMainQueue() {
+        super.startGyroUpdatesToQueue(NSOperationQueue.mainQueue()) { (data, error) in
+            self.delegate?.gyroUpdated(data)
+        }
+    }
+    
+    /** Gyro data will treat by handler whenever it updates.
+     */
+    public func startGyroUpdatesToMainQueue(handler: CMGyroHandler) -> Bool {
+        if !self.gyroAvailable {
+            return false
+        } else {
+            super.startGyroUpdatesToQueue(NSOperationQueue.mainQueue(), withHandler: handler)
+            return true
+        }
+    }
+
+
+    
 }
 
 public protocol SwiftySensorControlDelegate: class {
     func accelerometerUpdated(accellerometerData: CMAccelerometerData?)
+    func gyroUpdated(gyroData: CMGyroData?)
+}
+
+extension CMAcceleration {
+    /** Only for average value given device regardless of direction.
+     *  It can be used to find how much power is given to device.
+     *  - return: sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2))
+     */
+    var integratedData: Double {
+        return sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2))
+    }
+}
+
+extension CMRotationRate {
+    /** Only for average value given device regardless of direction.
+     *  It can be used to find how much device rotated.
+     *  - return: sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2))
+     */
+    var integratedData: Double {
+        return sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2))
+    }
 }
