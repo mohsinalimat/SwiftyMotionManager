1ab6c887bddaed212604187323b102fc0b434bb7
Jeff Kang <sandfox2010@gmail.com>
2016-09-18 23:24:08 +0900

    Add a magnetometer example.

---
+++ Example/Example/MagnetometerViewController.swift
@@ -0,0 +1,43 @@
+//
+//  MagnetometerViewController.swift
+//  Example
+//
+//  Created by Jeff Kang on 9/18/16.
+//  Copyright © 2016 jeffgukang. All rights reserved.
+//
+
+import UIKit
+import SwiftyMotionManager
+import CoreMotion
+
+class MagnetometerViewController: UIViewController {
+    
+    @IBOutlet weak var xLabel: UILabel!
+    @IBOutlet weak var yLabel: UILabel!
+    @IBOutlet weak var zLabel: UILabel!
+    @IBOutlet weak var aveLabel: UILabel!
+    
+    let swiftyMotionManager = SwiftyMotionManager.sharedInstance
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        xLabel.adjustsFontSizeToFitWidth = true
+        yLabel.adjustsFontSizeToFitWidth = true
+        zLabel.adjustsFontSizeToFitWidth = true
+        aveLabel.adjustsFontSizeToFitWidth = true
+        
+        swiftyMotionManager.startMagnetometerUpdatesToMainQueue { (magnetometerData, error) in
+            self.xLabel.text = magnetometerData?.magneticField.x.description
+            self.yLabel.text = magnetometerData?.magneticField.y.description
+            self.zLabel.text = magnetometerData?.magneticField.z.description
+            self.aveLabel.text = magnetometerData?.magneticField.integratedData.description
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
