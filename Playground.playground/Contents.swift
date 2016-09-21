deaeaace60fd264b2485313247ecdf8404388037
Jeff Kang <sandfox2010@gmail.com>
2016-09-22 00:16:24 +0900

    Update Playground.

--- Playground.playground/Contents.swift
+++ Playground.playground/Contents.swift
@@ -1,9 +1,10 @@
 //: Playground - noun: a place where people can play
 
+//If you want to use this playground, you have to build your framework for iOS simulator.
 import SwiftyMotionManager
 
 var str = "Hello, playground"
 
 let control = SwiftyMotionManager()
-control.startAccelerometerUpdatesIfItCan()
+control.startGyroUpdatesIfAvailable()
 
